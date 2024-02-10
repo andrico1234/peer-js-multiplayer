663ae779370941e546126fc38bfe57001a3064f7
Andrico <karoulla.andrico@gmail.com>
2024-02-10 19:57:07 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import { PeerServer } from 'peer';
+
+const PORT = 9002;
+
+const peerServer = PeerServer({
+  port: PORT,
+  key: 'demodemo',
+  path: '/myapp',
+  allow_discovery: true,
+});
+
+peerServer.on('connection', (client) => {
+  console.log("connection ID:", client.id)
+})
+
+peerServer.on('disconnect', (client) => {
+  console.log("disconnect ID:", client.id)
+})
+
+console.log(`Peer server running on port ${PORT}...`)
